7c127aa7a1781a185497ebec3f90da8fb26392df
fika123 <prandika775@gmail.com>
2024-11-26 10:19:51 +0700

    first commit

---
+++ ellaravel/app/Http/Controllers/SessionController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Sessiomn;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+
+class SessionController extends Controller
+{
+    public function index(){
+        return view('sesi/index');
+    }
+}
---
+++ ellaravel/resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Sisfo pegawai created By Elfata Hilmi </span>
+        </div>
+    </div>
+</footer>
